cf585a8b084fff220b003247f85786f85a394454
OksanaVinichuk <OViniTheChuk@gmail.com>
2021-01-28 09:03:59 +0200

    lecture forms #form="ngForm"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'lectures';
 
-  onInputAction(el: HTMLInputElement): void {
-console.log(this.title)
+export class AppComponent {
+  user = {name: 'vasya', age: 123};
+
+  submit(form: NgForm): void {
+    console.log(form);
   }
 }
 
