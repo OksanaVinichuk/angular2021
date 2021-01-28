5ac5fc288cf85b33a5204dcddbbb98faaa759f93
OksanaVinichuk <OViniTheChuk@gmail.com>
2021-01-28 11:57:02 +0200

    lecture forms ReactiveFormsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-import {NgForm} from '@angular/forms';
+// import {NgForm} from '@angular/forms';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,26 @@ import {NgForm} from '@angular/forms';
 })
 
 export class AppComponent {
-  user = {name: 'andrew', age: 456};
+//   user = {name: 'andrew', age: 456};
+//
+//   submit(form: NgForm): void {
+//     console.log(form);
+//   }
 
-  submit(form: NgForm): void {
-    console.log(form);
+name = new FormControl('', [Validators.required, Validators.minLength(3), this.customFukcValidator]);
+age = new FormControl('456');
+myForm = new FormGroup({
+  name: this.name,
+  age: this.age
+});
+fff(myForm: FormGroup): void {
+    console.log(myForm);
   }
+customFukcValidator(inputData: AbstractControl): any{
+ if (inputData.value === 'fukc'){
+   console.log(inputData);
+   return {error: true, msg: 'fukc is present'};
+ } else {return null; }
+}
 }
 
