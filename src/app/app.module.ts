605f99e6b6a05dcef9156c8b1fad8b3e084c2157
OksanaVinichuk <OViniTheChuk@gmail.com>
2021-01-28 19:44:32 +0200

    dz_forms_0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { UsersDataComponent } from './components/users-data/users-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersDataComponent
   ],
     imports: [
         BrowserModule,
